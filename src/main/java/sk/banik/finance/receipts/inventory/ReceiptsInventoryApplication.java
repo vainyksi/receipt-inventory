33ec0faa2f832542885c4ed7424de2b22e78f675
Michal Banik <banik.michal.sk@gmail.com>
2023-10-12 21:27:08 +0200

    add empty springboot app
    with:
    Spring Web
    Spring Data Mongo DB
    Testcontainers
    Spring Boot DevTools

---
+++ src/main/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplication.java
@@ -0,0 +1,13 @@
+package sk.banik.finance.receipts.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptsInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.banik.finance.receipts.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
