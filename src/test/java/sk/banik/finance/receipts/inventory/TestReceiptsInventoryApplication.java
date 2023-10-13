2a63f2e6e47cca6a5d29ac8577b39f6e50082089
Michal Banik <banik.michal.sk@gmail.com>
2023-10-13 11:24:18 +0200

    replace MongoDB by postgresql dependency

--- src/test/java/sk/banik/finance/receipts/inventory/TestReceiptsInventoryApplication.java
+++ src/test/java/sk/banik/finance/receipts/inventory/TestReceiptsInventoryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,8 +12,8 @@ public class TestReceiptsInventoryApplication {
 
     @Bean
     @ServiceConnection
-    MongoDBContainer mongoDbContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
     }
 
     public static void main(String[] args) {
