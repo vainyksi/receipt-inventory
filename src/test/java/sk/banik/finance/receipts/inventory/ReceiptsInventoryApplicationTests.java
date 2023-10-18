a2a2c8cca6f69afba2b0e366379f37a90603c400
Michal Banik <banik.michal.sk@gmail.com>
2023-10-18 23:30:13 +0200

    in tests use testcontainers only

--- src/test/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplicationTests.java
+++ src/test/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplicationTests.java
@@ -2,9 +2,24 @@ package sk.banik.finance.receipts.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 class ReceiptsInventoryApplicationTests {
+
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @ServiceConnection
+        PostgreSQLContainer<?> postgresContainer() {
+            return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        }
+    }
 
     @Test
     void contextLoads() {
