7bdfd0879b1f5166955a55723da57b2caef419d2
Michal Banik <banik.michal.sk@gmail.com>
2023-10-18 22:44:12 +0200

    use testcontainers for tests and docker-compose for runtime

--- src/test/java/sk/banik/finance/receipts/inventory/TestReceiptsInventoryApplication.java
+++ src/test/java/sk/banik/finance/receipts/inventory/TestReceiptsInventoryApplication.java
@@ -1,23 +1,39 @@
 package sk.banik.finance.receipts.inventory;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.stream.Collectors;
+
 @TestConfiguration(proxyBeanMethods = false)
+@SpringBootTest
 public class TestReceiptsInventoryApplication {
 
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @ServiceConnection
+        PostgreSQLContainer<?> postgresContainer() {
+            return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        }
     }
 
-    public static void main(String[] args) {
-        SpringApplication.from(ReceiptsInventoryApplication::main).with(TestReceiptsInventoryApplication.class).run(args);
+    @Test
+    void contextLoads(@Autowired JdbcTemplate template) {
+        template
+                .query("select * from receipts",
+                        (rs, rowNum) -> new Receipt(rs.getInt("id"), rs.getString("code")))
+                .forEach(System.out::println);
     }
 
 }
