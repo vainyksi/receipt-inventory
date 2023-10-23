3d64dbe8755c7d4dbc65f2c8e3d4cf7f6a483421
Michal Banik <banik.michal.sk@gmail.com>
2023-10-23 13:30:55 +0200

    init schema and load receipts as example
    init schema if not exist from entity classes
     and load stored receipts from DB as an example

--- src/main/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplication.java
+++ src/main/java/sk/banik/finance/receipts/inventory/ReceiptsInventoryApplication.java
@@ -1,7 +1,12 @@
 package sk.banik.finance.receipts.inventory;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import sk.banik.finance.receipts.inventory.model.Receipt;
 
 @SpringBootApplication
 public class ReceiptsInventoryApplication {
@@ -10,4 +15,20 @@ public class ReceiptsInventoryApplication {
         SpringApplication.run(ReceiptsInventoryApplication.class, args);
     }
 
+}
+
+@Configuration
+class JdbcConfiguration {
+    @Bean
+    public ApplicationRunner exampleOfDataReading(JdbcTemplate template){
+        return args -> template
+                .query("select * from receipt",
+                        (rs, rowNum) -> {
+                            Receipt receipt = new Receipt();
+                            receipt.setId(rs.getLong("id"));
+                            receipt.setCode(rs.getString("code"));
+                            return receipt;
+                        })
+                .forEach(System.out::println);
+    }
 }
